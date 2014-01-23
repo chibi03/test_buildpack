d67b9092e0110129bc76eb6c155a1195e5816120
chibi03 <saa175@bham.ac.uk>
2014-01-23 16:25:29 +0000

    Add condition to prevent nil from appearing

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -70,7 +70,11 @@ module LibertyBuildpack::Container
         apps_found = [@app_dir]
       elsif Liberty.meta_inf(@app_dir)
         apps_found = [@app_dir]
-        ['*.war', '*.ear'].each { |suffix| exp_app += Dir.glob(File.expand_path(File.join(@app_dir, suffix))) } # rubocop:disable all
+        ['*.war', '*.ear'].each do |suffix|
+          tmp = Dir.glob(File.expand_path(File.join(@app_dir, suffix))) # rubocop:disable all
+          exp_app += tmp unless tmp == nil
+          @logger.info("#{exp_app}")
+        end
         Liberty.expand_apps(exp_app)
       elsif server_xml
         ['*.war', '*.ear', '*.eba'].each { |suffix| apps_found += Dir.glob(File.expand_path(File.join(server_xml, '..', '**', suffix))) }
